57329e4c232127a3d9f7fcae10de08ca080a0495
Chrys Rakotonimanana <chrysrakk@gmail.com>
2020-04-10 21:59:56 +0300

    refactoring

--- index.js
+++ index.js
@@ -8,32 +8,30 @@ const express = require('express'),
 app.use(bodyParser.json());
 
 app.route('/webhook')
-	.post((req, res)=>{
-	let body = req.body;
-	if(body.object === 'page'){
-		body.entry.forEach((entry)=>{
-			let webhook_event = entry.messaging[0];
-            let sender_psid = webhook_event.sender.id;
-            console.log(webhook_event);
-            console.log('Sender PSID: ' + sender_psid);
-            if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message);
-            } else if (webhook_event.postback) {
-                handlePostback(sender_psid, webhook_event.postback);
-            }
-		});
-	return res.status(200).send('EVENT_RECEIVED');
-	}
-	return res.sendStatus(404);
-	}).get((req, res) => {
-		let VERIFY_TOKEN = process.env.VERIFY_TOKEN || '';
-		let mode = req.query['hub.mode'];
-		let token = req.query['hub.verify_token'];
-		let challenge = req.query['hub.challenge'];
+    .post((req, res) => {
+        const {body} = req;
+        if (body.object === 'page') {
+            body.entry.forEach((entry) => {
+                let webhook_event = entry.messaging[0];
+                let sender_psid = webhook_event.sender.id;
+                if (webhook_event.message) {
+                    handleMessage(sender_psid, webhook_event.message);
+                } else if (webhook_event.postback) {
+                    handlePostback(sender_psid, webhook_event.postback);
+                }
+            });
+            return res.status(200).send('EVENT_RECEIVED');
+        }
+        return res.sendStatus(404);
+    })
+    .get((req, res) => {
+        const VERIFY_TOKEN = process.env.VERIFY_TOKEN || '';
+        const mode = req.query['hub.mode'];
+        const token = req.query['hub.verify_token'];
+        const challenge = req.query['hub.challenge'];
 
 		if(mode && token){
 			if(mode === 'subscribe' && token === VERIFY_TOKEN){
-				console.log('WEBHOOK_VERIFIED');
 				return res.status(200).send(challenge);
 			}
 			return res.sendStatus(403);
@@ -41,14 +39,14 @@ app.route('/webhook')
 	});
 
 function handleMessage(sender_psid, received_message) {
-
-    let response;
+    let response = {};
     if (received_message.text) {
         response = {
             "text": `Ito ny message nalefanao: "${received_message.text}". Andefaso sary hoe za zao!`
         }
-    } else if (received_message.attachments) {
-        let attachment_url = received_message.attachments[0].payload.url;
+    }
+    else if (received_message.attachments) {
+        const attachment_url = received_message.attachments[0].payload.url;
         response = {
             "attachment": {
                 "type": "template",
@@ -79,7 +77,7 @@ function handleMessage(sender_psid, received_message) {
 }
 
 function handlePostback(sender_psid, received_postback) {
-    let response;
+    let response = {};
     let payload = received_postback.payload;
     if (payload === 'yes') {
         response = { "text": "Misaotra!" }
@@ -90,7 +88,7 @@ function handlePostback(sender_psid, received_postback) {
 }
 
 function callSendAPI(sender_psid, response) {
-    let request_body = {
+    const request_body = {
         "recipient": {
             "id": sender_psid
         },
@@ -99,7 +97,7 @@ function callSendAPI(sender_psid, response) {
 
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": {"access_token": process.env.PAGE_ACCESS_TOKEN},
+        "qs": {"access_token": process.env.PAGE_ACCESS_TOKEN || ''},
         "method": "POST",
         "json": request_body
     }, (err, res, body) => {
@@ -110,4 +108,5 @@ function callSendAPI(sender_psid, response) {
         }
     });
 }
-app.listen(process.env.PORT || 1337, ()=>console.log('webhook is listnening'));
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listnening'));
