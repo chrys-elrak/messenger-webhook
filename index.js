7df4d795f2a1d2a2db3ccdadec53398e93030ad2
Chrys Rakotonimanana <chrysrakk@gmail.com>
2020-04-10 21:42:21 +0300

    making bot response

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express'),
 	bodyParser = require('body-parser'),
+    request = require('request'),
 	app = express();
 
 app.use(bodyParser.json());
@@ -12,7 +13,14 @@ app.route('/webhook')
 	if(body.object === 'page'){
 		body.entry.forEach((entry)=>{
 			let webhook_event = entry.messaging[0];
-			console.log(webhook_event);
+            let sender_psid = webhook_event.sender.id;
+            console.log(webhook_event);
+            console.log('Sender PSID: ' + sender_psid);
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
 		});
 	return res.status(200).send('EVENT_RECEIVED');
 	}
@@ -32,4 +40,41 @@ app.route('/webhook')
 		}
 	});
 
+function handleMessage(sender_psid, received_message) {
+
+    let response;
+    if (received_message.text) {
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+    callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+function callSendAPI(sender_psid, response) {
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    };
+
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": {"access_token": process.env.PAGE_ACCESS_TOKEN},
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
 app.listen(process.env.PORT || 1337, ()=>console.log('webhook is listnening'));
